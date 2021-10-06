437e862f245a805a3316edc08ad0d8a2aab81e42
Luboš Mátl <matllubos@gmail.com>
2021-10-06 13:35:05 +0200

    Fixed django run celery command

--- django_celery_extensions/management/commands/run_celery.py
+++ django_celery_extensions/management/commands/run_celery.py
@@ -1,20 +1,34 @@
+import os
+
 import shlex
 import subprocess
 
 from django.conf import settings as django_settings
 from django.core.management.base import BaseCommand
-from django.utils.autoreload import run_with_reloader
+from django.utils.autoreload import run_with_reloader, DJANGO_AUTORELOAD_ENV
+
+import signal
 
 
-def start_celery(celery_type, celery_settings, extra_arguments, autoreload=False):
-    starter_celery_cmd = 'celery --app {} {} {}'.format(celery_settings, celery_type, extra_arguments)
-    if celery_type == 'worker' and hasattr(django_settings, 'CELERY_LISTEN_QUEUES'):
-        starter_celery_cmd += ' -Q {}'.format(django_settings.CELERY_LISTEN_QUEUES)
+def kill_celery(celery_type, celery_settings):
+    kill_celery_command = f'pkill -9 -f "{get_celery_command(celery_type, celery_settings, add_queue=False)}"'
+    subprocess.call(shlex.split(kill_celery_command))
 
-    if autoreload:
-        kill_worker_cmd = 'pkill -9 -f "{}"'.format(starter_celery_cmd)
-        subprocess.call(shlex.split(kill_worker_cmd))
-    subprocess.call(shlex.split(starter_celery_cmd))
+
+def autoreload_celery(celery_type, celery_settings, extra_arguments):
+    kill_celery(celery_type, celery_settings)
+    subprocess.call(shlex.split(get_celery_command(celery_type, celery_settings, extra_arguments)))
+
+
+def get_celery_command(celery_type, celery_settings, extra_arguments=None, add_queue=True):
+    celery_cmd = f'celery --app {celery_settings} {celery_type}'
+    if extra_arguments:
+        celery_cmd += f' {extra_arguments}'
+
+    if add_queue and celery_type == 'worker' and hasattr(django_settings, 'CELERY_LISTEN_QUEUES'):
+        listen_queues = ' '.join(django_settings.CELERY_LISTEN_QUEUES)
+        celery_cmd += f' -Q {listen_queues}'
+    return celery_cmd
 
 
 class Command(BaseCommand):
@@ -37,19 +51,22 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        celery_type = options.get('type')
+        celery_settings = options.get('celery_settings')
+        extra_arguments = options.get('extra_args', '')
         if options.get('use_reloader'):
-            self.stdout.write('Starting celery with autoreload...')
+            if os.environ.get(DJANGO_AUTORELOAD_ENV) != 'true':
+                self.stdout.write('Starting celery with autoreload...')
             run_with_reloader(
-                start_celery,
-                celery_type=options.get('type'),
-                celery_settings=options.get('celery_settings'),
-                extra_arguments=options.get('extra_args', ''),
-                autoreload=True
+                autoreload_celery,
+                celery_type=celery_type,
+                celery_settings=celery_settings,
+                extra_arguments=extra_arguments
             )
         else:
             self.stdout.write('Starting celery...')
-            start_celery(
-                celery_type=options.get('type'),
-                celery_settings=options.get('celery_settings'),
-                extra_arguments=options.get('extra_args', '')
+            process = subprocess.Popen(
+                shlex.split(get_celery_command(celery_type, celery_settings, extra_arguments))
             )
+            signal.signal(signal.SIGTERM, lambda *args: process.terminate())
+            process.wait()
