f747136030d5d77d1856ac537ab3cd9cc4f0594a
Luboš Mátl <matllubos@gmail.com>
2021-10-15 11:57:48 +0200

    0.0.24

--- django_celery_extensions/__init__.py
+++ django_celery_extensions/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Luboš Mátl"""
 __email__ = 'matllubos@gmail.com'
-__version__ = '0.0.23'
+__version__ = '0.0.24'
 
 
 default_app_config = 'django_celery_extensions.apps.DjangoCeleryExtensionsAppConfig'
