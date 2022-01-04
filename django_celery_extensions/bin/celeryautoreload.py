786d61afac7660474b2262eaf16cd450354e1857
Luboš Mátl <matllubos@gmail.com>
2022-01-04 15:12:51 +0100

    Added better command for run celery

--- django_celery_extensions/bin/celeryautoreload.py
+++ django_celery_extensions/bin/celeryautoreload.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 import shlex
@@ -8,36 +7,15 @@ from django.utils.autoreload import run_with_reloader
 from celery.bin.celery import celery
 
 
-def autoreload_celery(celery_type, celery_settings, extra_arguments):
-    celery(shlex.split(get_celery_command(
-        celery_type=celery_type,
-        celery_pool='solo',
-        celery_settings=celery_settings,
-        extra_arguments=extra_arguments
-    )))
-
-
-def get_celery_command(celery_type, celery_pool, celery_settings, extra_arguments=None, add_queue=True):
-    celery_cmd = f'--app {celery_settings} {celery_type}'
-    if extra_arguments:
-        celery_cmd += f' {extra_arguments}'
-    if celery_type == 'worker':
-        if celery_pool:
-            celery_cmd += f' --pool {celery_pool}'
-
-        if add_queue:
-            listen_queues = 'fast.fifo'
-            celery_cmd += f' -Q {listen_queues}'
-    return celery_cmd
+def autoreload_celery():
+    command = shlex.split(' '.join(sys.argv[1:]))
+    if 'worker' in command:
+        command.append('--pool=solo')
+    celery(command)
 
 
 def celery_autoreload():
-    run_with_reloader(
-        autoreload_celery,
-        celery_type='worker',
-        celery_settings='common.celery',
-        extra_arguments='-Ofair --concurrency 2 --loglevel INFO'
-    )
+    run_with_reloader(autoreload_celery)
 
 
 if __name__ == '__main__':
